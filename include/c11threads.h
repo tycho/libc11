30311281c6539efdc07114e670d4080e41942bbb
Steven Noonan <steven@uplinklabs.net>
2014-03-11 05:03:53 -0700

    implement C11 threads.h mutexes
    Signed-off-by: Steven Noonan <steven@uplinklabs.net>

--- include/c11threads.h
+++ include/c11threads.h
@@ -6,13 +6,21 @@
 #include <time.h>
 
 typedef uintptr_t thrd_t;
+typedef uintptr_t mtx_t;
 
 typedef int (*thrd_start_t)(void *);
 
 enum {
     thrd_success = 0,
     thrd_error = 1,
+    thrd_busy = 2,
     thrd_nomem = 3
+};
+
+enum {
+    mtx_plain = 1,
+    mtx_timed = 2,
+    mtx_recursive = 4
 };
 
 __BEGIN_DECLS
@@ -25,6 +33,13 @@ _Noreturn void thrd_exit(int);
 int    thrd_join(thrd_t, int *);
 int    thrd_sleep(const struct timespec *, struct timespec *);
 void   thrd_yield(void);
+
+int    mtx_init(mtx_t *, int);
+int    mtx_lock(mtx_t *);
+int    mtx_timedlock(mtx_t *restrict, const struct timespec *restrict);
+int    mtx_trylock(mtx_t *);
+int    mtx_unlock(mtx_t *);
+void   mtx_destroy(mtx_t *);
 
 __END_DECLS
 
--- src/pthread.c
+++ src/pthread.c
@@ -65,4 +65,121 @@ void thrd_yield(void)
     pthread_yield();
 }
 
+int mtx_init(mtx_t *_mtx, int _type)
+{
+    int rv = thrd_success;
+    pthread_mutexattr_t attr;
+    pthread_mutex_t *mtx;
+
+    if (!_mtx)
+        return thrd_error;
+
+    /* mtx_plain and mtx_timed are mutually exclusive options, and exactly one
+     * of the two must be set.
+     */
+    if (!((_type & mtx_plain) ^ (_type & mtx_timed)))
+        return thrd_error;
+
+    if (pthread_mutexattr_init(&attr)) {
+        if (errno == ENOMEM)
+            rv = thrd_nomem;
+        else
+            rv = thrd_error;
+        goto error0;
+    }
+
+    if ((_type & mtx_plain) && pthread_mutexattr_settype(&attr, PTHREAD_MUTEX_NORMAL)) {
+        rv = thrd_error;
+        goto error1;
+    }
+
+    if ((_type & mtx_timed) && pthread_mutexattr_settype(&attr, PTHREAD_MUTEX_TIMED_NP)) {
+        rv = thrd_error;
+        goto error1;
+    }
+
+    if ((_type & mtx_recursive) && pthread_mutexattr_settype(&attr, PTHREAD_MUTEX_RECURSIVE)) {
+        rv = thrd_error;
+        goto error1;
+    }
+
+    mtx = malloc(sizeof(pthread_mutex_t));
+    if (!mtx) {
+        rv = thrd_nomem;
+        goto error1;
+    }
+
+    if (pthread_mutex_init(mtx, &attr)) {
+        rv = thrd_error;
+        goto error2;
+    }
+
+    *_mtx = (uintptr_t)mtx;
+    mtx = NULL;
+
+error2:
+    free(mtx);
+error1:
+    pthread_mutexattr_destroy(&attr);
+error0:
+    return rv;
+}
+
+int mtx_lock(mtx_t *_mtx)
+{
+    pthread_mutex_t *mtx = (pthread_mutex_t *)*_mtx;
+    switch (pthread_mutex_lock(mtx)) {
+    case EDEADLK:
+        return thrd_busy;
+    case 0:
+        return thrd_success;
+    default:
+        return thrd_error;
+    }
+}
+
+int mtx_timedlock(mtx_t *restrict _mtx, const struct timespec *restrict _ts)
+{
+    pthread_mutex_t *mtx = (pthread_mutex_t *)*_mtx;
+    switch (pthread_mutex_timedlock(mtx, _ts)) {
+    case EBUSY:
+        return thrd_busy;
+    case 0:
+        return thrd_success;
+    default:
+        return thrd_error;
+    }
+}
+
+int mtx_trylock(mtx_t *_mtx)
+{
+    pthread_mutex_t *mtx = (pthread_mutex_t *)*_mtx;
+    switch (pthread_mutex_trylock(mtx)) {
+    case EBUSY:
+        return thrd_busy;
+    case 0:
+        return thrd_success;
+    default:
+        return thrd_error;
+    }
+}
+
+int mtx_unlock(mtx_t *_mtx)
+{
+    pthread_mutex_t *mtx = (pthread_mutex_t *)*_mtx;
+    switch (pthread_mutex_unlock(mtx)) {
+    case 0:
+        return thrd_success;
+    default:
+        return thrd_error;
+    }
+}
+
+void mtx_destroy(mtx_t *_mtx)
+{
+    pthread_mutex_t *mtx = (pthread_mutex_t *)*_mtx;
+    pthread_mutex_destroy(mtx);
+    free(mtx);
+}
+
 /* vim: set ts=4 sw=4 noai expandtab: */
